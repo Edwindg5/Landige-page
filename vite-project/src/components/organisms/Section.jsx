8cdef774253dc949a16b8669aac7e5c69a9c9054
Edwindg5 <152365021+Edwindg5@users.noreply.github.com>
2024-07-18 20:51:03 -0600

    commit

--- vite-project/src/components/organisms/Section.jsx
+++ vite-project/src/components/organisms/Section.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import Title from '../atoms/Title';
-import Gallery from '../molecules/Gallery';
-import haloData from '../../data/haloData';
+import Grid from '../molecules/Grid';
 import styles from './Section.module.css';
 
 function Section() {
-  const games = haloData.products.map(product => ({
-    src: `/src/assets/images/${product.image}`,
-    alt: product.text,
-  }));
-
   return (
     <section className={styles.section}>
       <Title text="Explora los Juegos de Halo" />
-      <Gallery images={games} />
+      <Grid />
     </section>
   );
 }
