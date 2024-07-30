a075188c983e983e49244d3f67bf6fe7aa248a2c
Edwindg5 <152365021+Edwindg5@users.noreply.github.com>
2024-07-30 09:08:52 -0600

    Delete vite-project directory

--- vite-project/src/components/atoms/Image.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Image.module.css';
-
-function Image({ src, alt, className }) {
-  return <img src={src} alt={alt} className={className} />;
-}
-
-export default Image;
